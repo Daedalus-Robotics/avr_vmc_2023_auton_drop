509d3e1f7f7fc7423bd287be59ba4aa208be47ef
literallyanything <hunterbaker@me.com>
2023-11-19 15:36:26 -0500

    Swap to using an action
    Co-authored-by: ay2609 <ayadava@ideaventionsacademy.org>

--- avr_vmc_2023_auton_drop/auton_drop.py
+++ avr_vmc_2023_auton_drop/auton_drop.py
@@ -1,9 +1,15 @@
+from typing import List
+
+import time
 import rclpy
+from rclpy.action.server import ServerGoalHandle
 from rclpy.node import Node
+from rclpy.action import ActionServer, CancelResponse, GoalResponse
 from rclpy.qos import QoSPresetProfiles
 from avr_pcc_2023_interfaces.srv import SetLedStrip
-from std_srvs.srv import Trigger, SetBool
-from apriltag_msgs.msg import AprilTagDetectionArray
+from std_srvs.srv import Trigger
+from apriltag_msgs.msg import AprilTagDetectionArray, AprilTagDetection
+from avr_vmc_2023_auton_drop_interfaces.action import AutonDrop
 
 
 class AutonDropNode(Node):
@@ -16,13 +22,19 @@ class AutonDropNode(Node):
 
         self.enabled = False
         self.should_drop = False
+        self.apriltag: List[AprilTagDetection] = []
 
-        self.enable_service = self.create_service(
-            # custom message type,
-            SetBool,
-            'enable',
-            self.enable
+        self.auton_trigger_client = ActionServer(
+            self,
+            Trigger,
+            'trigger',
+            execute_callback=self.execute_callback,
+            goal_callback=self.goal_callback,
+            handle_accepted_callback=self.handle_accepted_callback,
+            cancel_callback=self.cancel_callback
         )
+
+        # when you call it, it will start searching for a tag,
 
         self.trigger_client = self.create_client(
             Trigger,
@@ -53,23 +65,50 @@ class AutonDropNode(Node):
         self.get_logger().info('Waiting for set LED strip service')
         self.trigger_client.wait_for_service()
 
-    def enable(self, request: SetBool, response: SetBool) -> SetBool:
-        enable_request = request.Request
-        enable_response = response.Response
+    # send one empty feedback message when we find an apriltag,
 
-        self.enabled = enable_request.data
-        self.should_drop = enable_request.data
+    # send result after we move servo
 
-        enable_response.success = True
+    def goal_callback(self, goal_request: AutonDrop.Goal) -> GoalResponse:
+        if not self.enabled:
+            self.get_logger().info('Started auton drop')
+        else:
+            self.get_logger().warn('Tried to trigger an auton drop while already running')
+        return GoalResponse.ACCEPT if not self.enabled else GoalResponse.REJECT
 
-    def apriltag_callback(self, detections: AprilTagDetectionArray) -> None:
-        if self.enabled:
-            if len(detections.detections) >= 1:
-                # detection = detections.detections[0] # I don't think we even need this right now
+    def handle_accepted_callback(self, goal_handle: ServerGoalHandle) -> None:
+        self.enabled = True
+        goal_handle.execute()
 
-                # blink LEDs 3 times
+    def cancel_callback(self, _) -> CancelResponse:
+        self.get_logger().debug('Received cancel request')
+        return CancelResponse.ACCEPT
+
+    def execute_callback(self, goal_handle: ServerGoalHandle):
+        # ToDo: Add Barriers or Events or something to synchronize two callbacks
+
+        self.get_logger().info('Executing auton drop...')
+        request_msg: AutonDrop.Goal = goal_handle.request
+        feedback_msg = AutonDrop.Feedback()
+        result_msg = AutonDrop.Result()
+
+        while True:
+            time.sleep(1 / 12)
+            if not goal_handle.is_active:
+                self.get_logger().info('Drop aborted')
+                break
+
+            if goal_handle.is_cancel_requested:
+                goal_handle.canceled()
+                self.get_logger().info('Drop canceled')
+                break
+
+            if len(self.apriltag) >= 1:
+                goal_handle.publish_feedback(feedback_msg)
+
+                self.get_logger().info('Apriltag found, flashing LEDs')
+
                 led_request = SetLedStrip.Request()
-
                 led_request.mode = SetLedStrip.Request.MODE_FLASH
                 led_request.argument = 3
                 led_request.color.r = 255
@@ -78,15 +117,24 @@ class AutonDropNode(Node):
 
                 led_future = self.led_client.call_async(led_request)
 
-                if self.should_drop:
-                    led_future.add_done_callback(lambda: self.drop_timer.reset()) # <- in this callback add the timer
-                    trigger_request = Trigger.Request()
-                    self.trigger_client.call_async(trigger_request)
+                if request_msg.should_drop:
+                    led_future.add_done_callback(lambda: self.drop_timer.reset())  # <- in this callback add the timer
 
-                # send trigger through client (don't need to worry about full or half state here)
+                break
+
+        goal_handle.succeed()
+        self.enabled = False
+        return AutonDrop.Result()
+
+    def apriltag_callback(self, detections: AprilTagDetectionArray) -> None:
+        self.apriltag = detections.detections
 
     def do_drop(self) -> None:
         self.drop_timer.cancel()
+
+        self.get_logger().info('Triggering drop')
+        trigger_request = Trigger.Request()
+        self.trigger_client.call_async(trigger_request)
 
 
 def main() -> None:
